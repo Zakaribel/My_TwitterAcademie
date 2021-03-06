20c54e7d6319090aed06c9b10aca40a23bc34312
unknown <zakaria.belkacemi@epitech.eu>
2021-03-06 16:15:59 +0100

    paufinage

--- js/sr.js
+++ js/sr.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function () {
   let togg1 = $("#togg1");
   let togg2 = $("#togg2");
@@ -14,20 +13,3 @@ $(document).ready(function () {
     d2.css("display", "none");
   });
 });
-=======
-$(document).ready(function () {
-  let togg1 = $("#togg1");
-  let togg2 = $("#togg2");
-  let d1 = $("#d1");
-  let d2 = $("#d2");
-
-  togg2.on("click", function () {
-    d1.css("display", "none");
-    d2.css("display", "block");
-  });
-  togg1.on("click", function () {
-    d1.css("display", "block");
-    d2.css("display", "none");
-  });
-});
->>>>>>> b222b1be7e7f9610cd08d1c1f1f7b4c078855ccb
--- views/logout.php
+++ views/logout.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 session_start();
 
@@ -6,15 +5,4 @@ session_destroy();
 
 header('location:./login.php');
 
-=======
-<?php
-
-
-session_start();
-
-session_destroy();
-
-header('location:./login.php');
-
->>>>>>> b222b1be7e7f9610cd08d1c1f1f7b4c078855ccb
 ?>
